d2d097b8cd38322b2c74548b1876b08faaec4819
nashshhshh <151946783+nashshsh1@users.noreply.github.com>
2024-12-07 07:03:16 +0800

    first commit

--- nashvel/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
-
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/users', [UserController::class, 'user']);
